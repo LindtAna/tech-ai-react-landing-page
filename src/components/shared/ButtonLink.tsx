8c5c8fdcaed4124be81a004274c2d817159b524c
Lindt <anastasiialindt@gmail.com>
2025-05-25 14:28:36 +0200

    Fixed buttons

--- src/components/shared/ButtonLink.tsx
+++ src/components/shared/ButtonLink.tsx
@@ -7,7 +7,7 @@ interface ButtonLinkProps {
 export const ButtonLink = ({ href, text, className="" }: ButtonLinkProps) => {
   return (
    <a href={href}
-   className={`px-6 py-3 rounded-full outline-none relative border dark:bg-violet-600 cursor-pointer transform transition duration-300 hover:scale-[1.05] origin-center ${className}`}>
+   className={`px-6 py-3 rounded-full outline-none relative border bg-violet-600 cursor-pointer transform transition duration-300 hover:scale-[1.05] origin-center ${className}`}>
     <span className="relative z-10 text-white">{text}</span>
    </a>
   );
