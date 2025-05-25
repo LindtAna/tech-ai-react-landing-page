e038b4f893cba682240397a5c25c3ea69f2368ac
Lindt <anastasiialindt@gmail.com>
2025-05-25 14:33:39 +0200

    Fixed buttons

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -8,7 +8,7 @@ export const Button = ({ onClick, children, className="" }: ButtonProps) => {
   return (
    <button onClick={onClick}
    className={`group px-6 py-3 rounded-full outline-none cursor-pointer
-              relative overflow-hidden border border-violet-600 bg-violet-600 hover:bg-white transition-colors duration-300 ${className}`}>
+              relative overflow-hidden border border-violet-600 bg-violet-600  text-white hover:bg-white hover:text-violet-600 transition-colors duration-300 ${className}`}>
     {children}
    </button>
   );
